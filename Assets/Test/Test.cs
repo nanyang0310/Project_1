67aeecb408f8cc39eb1608024f3c7b3e4305022d
nanyang <836535111@qq.com>
2020-09-25 14:03:18 +0800

    第一次

---
+++ Assets/Test/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.ootii.Actors;
+using com.ootii.Cameras;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Test : MonoBehaviour
+{
+  public  CameraController m_cameraController;
+    public Transform m_cameraTargt;
+    public FirstPersonController m_firstPersonController;
+
+    void Start()
+    {
+        
+    }
+
+
+    void LateUpdate()
+    {
+        //m_cameraTargt.position = m_firstPersonController.transform.position;
+        //m_firstPersonController.transform.rotation = m_cameraTargt.rotation;
+
+        CameraMotor activeMotor = m_cameraController.ActiveMotor;
+        Debug.Log(activeMotor.Name);
+    }
+
+    public void SwitchToFirstPersonView()
+    {
+        CameraMotor activeMotor = m_cameraController.ActiveMotor;
+    }
+}
